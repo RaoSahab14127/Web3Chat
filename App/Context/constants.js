44a79cc64b162adba8cf47902a8d7636557642ce
Rao Hammad Ali <108279711+RaoSahab14127@users.noreply.github.com>
2023-06-20 14:21:34 +0500

    ok

--- App/Context/constants.js
+++ App/Context/constants.js
@@ -1,6 +1,6 @@
 //0x5FbDB2315678afecb367f032d93F642f64180aa3
 
 import ChatAppJSON  from "./ChatApp.json"; 
-export const ChatAppAddress = "0x2C8943F9945De1945D7Cb902f6F54aD4f44698bc";
+export const ChatAppAddress = "0xe6B9CAD957D146679b0BF9C8BeCb97a0FdAD86Dc";
 
 export const ChatAppABI = ChatAppJSON.abi;
--- App/hardhat.config.js
+++ App/hardhat.config.js
@@ -1,6 +1,22 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-toolbox" );
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+  solidity: {
+    compilers: [
+      {
+        version: '0.8.0',
+      },
+      {
+        version: '<0.9.0',
+        settings: {},
+      },
+    ],
+  },
+  networks: {
+    testnet_bitfinity: {
+      url: 'https://testnet.bitfinity.network',
+      accounts: ["0x77e233b4b4cbfe05ee92d5f798c2a5bbcdee02f3189fea8fab630ddf2625e4b0"],
+    },
+    
+  },
 };
